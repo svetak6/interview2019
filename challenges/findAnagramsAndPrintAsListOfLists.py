9d5adf3a900f7576e893ffa0d24fdc978ad975f2
svetak6 <svetak6@gmail.com>
2019-03-09 15:14:08 -0800

    add problem with solution

---
+++ challenges/findAnagramsAndPrintAsListOfLists.py
@@ -0,0 +1,29 @@
+# select anagrams in list of words and print as list of lists
+
+
+def detectMatchedWords(lSource):
+    dix = {}
+    for w in lSource:
+        curSorted = sorted(list(w))
+        sCurSortex = "".join(curSorted)
+
+        if dix.get(sCurSortex):
+            l = dix[sCurSortex]
+            l.append(w)
+            dix[sCurSortex ] =l
+        else:
+            dix[sCurSortex] = [w]
+
+    print(dix)
+    return dix
+
+def convertDictValuesToListOfLists(dix):
+    lResult = []
+    for key, it in dix.items():
+        lResult.append(it)
+
+    return lResult
+
+# Test
+lSource = ['eat' ,'tea' ,'ate', 'word' ,'rdow', 'rdwo', 'fgd hh', ' hhfgd']
+print(convertDictValuesToListOfLists(detectMatchedWords(lSource)))
